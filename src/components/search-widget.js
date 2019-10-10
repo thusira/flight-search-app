22489a77f3ebbe5ebd0cfdd82d9033874dd23b1c
Thusira Dissanayake <thusira.dissanayake@cinglevue.com>
2019-10-10 15:27:23 -0400

    UI changes

--- src/components/search-widget.js
+++ src/components/search-widget.js
@@ -9,11 +9,12 @@ export class SearchWidget extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            travelDate: moment(),
-            returnDate:moment()
+            travelDate: new Date(),
+            returnDate: new Date()
         };
         this.handleChangeTravel = this.handleChangeTravel.bind(this);
         this.handleChangeReturn = this.handleChangeReturn.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     handleChangeTravel(date) {
@@ -21,7 +22,7 @@ export class SearchWidget extends Component {
             travelDate: date
         })
     }
-    
+
     handleChangeReturn(date) {
         this.setState({
             returnDate: date
@@ -30,31 +31,32 @@ export class SearchWidget extends Component {
 
     handleSubmit(e) {
         e.preventDefault();
-        alert("Submit data");
+        alert(JSON.stringify(this.state));
     }
 
     render() {
         return (
 
-            <div class="container card card-body bg-light text-left">
+            <div className="container card card-body bg-light text-left">
                 <h3>Flight Search</h3>
                 <div className="row">
                     <div className="col-md-12">
-                        <div class="form-check-inline">
-                            <label class="form-check-label">
-                                <input type="radio" class="form-check-input" name="oneway" checked="checked"/>One way</label>
+                        <div className="form-check-inline">
+                            <label className="form-check-label">
+                                <input type="radio" className="form-check-input" name="oneway" checked="checked" />One way</label>
                         </div>
-                        <div class="form-check-inline">
-                            <label class="form-check-label">
-                                <input type="radio" class="form-check-input" name="return" />Return</label>
+                        <div className="form-check-inline">
+                            <label className="form-check-label">
+                                <input type="radio" className="form-check-input" name="return" />Return</label>
                         </div>
                     </div>
                     <div className="col-md-12">
                         <form onSubmit={this.handleSubmit}>
-                            <div class="form-row">
-                                <div class="form-group col-lg-3 col-md-3 col-sm-12">
-                                    <label for="from">From:</label>
-                                    <select class="form-control" id="from">
+                            <div className="form-row">
+                                <div className="form-group col-lg-3 col-md-3 col-sm-12">
+                                    <label htmlFor="from">From:</label>
+                                    <select className="form-control" id="from" onChange={(e) => this.setState({ from: e.target.value })}>
+                                        <option value=''></option>
                                         <option>YYR</option>
                                         <option>YYZ</option>
                                         <option>DXB</option>
@@ -62,9 +64,10 @@ export class SearchWidget extends Component {
                                         <option>JFK</option>
                                     </select>
                                 </div>
-                                <div class="form-group col-md-3 col-lg-3 col-sm-12">
-                                    <label for="to">To:</label>
-                                    <select class="form-control" id="to">
+                                <div className="form-group col-md-3 col-lg-3 col-sm-12">
+                                    <label htmlFor="to">To:</label>
+                                    <select className="form-control" id="to" onChange={(e) => this.setState({ to: e.target.value })}>
+                                        <option value=''></option>
                                         <option>YYR</option>
                                         <option>YYZ</option>
                                         <option>DXB</option>
@@ -72,31 +75,34 @@ export class SearchWidget extends Component {
                                         <option>JFK</option>
                                     </select>
                                 </div>
-                                <div class="form-group col-md-2 col-lg-2 col-sm-12">
-                                    <label for="to">Date:</label>
-                                    <p>
+                                <div className="form-group col-md-2 col-lg-2 col-sm-12">
+                                    <label htmlFor="to">Date:</label>
+                                    <div>
                                         <DatePicker
+                                            selected={this.state.travelDate}
                                             className="form-control"
                                             onChange={this.handleChangeTravel}
                                             name="travelDate"
-                                            dateFormat="MM/DD/YYYY"
-                                        />
-                                    </p>
-                                </div>
-                                <div class="form-group col-md-2 col-lg-2 col-sm-12">
-                                    <label for="to">Return Date:</label>
-                                    <div>
-                                        <DatePicker
-                                            className="form-control"
-                                            onChange={this.handleChangeReturn}
-                                            name="returnDate"
-                                            dateFormat="MM/DD/YYYY"
+                                            dateFormat="MM/dd/yyyy"
                                         />
                                     </div>
                                 </div>
-                                <div className="col-md-2 col-lg-2 col-sm-12 text-center justify-content-center align-self-center">
-                                    <button type="submit" class="btn btn-primary w-100">
-                                        <i class="fa fa-search" aria-hidden="true"></i></button>
+                                <div className="form-group col-md-2 col-lg-2 col-sm-12">
+                                    <label htmlFor="to">Return Date:</label>
+                                    <div>
+                                        <DatePicker
+                                            className="form-control"
+                                            selected={this.state.travelDate}
+                                            onChange={this.handleChangeReturn}
+                                            name="returnDate"
+                                            dateFormat="MM/dd/yyyy"
+                                        />
+                                    </div>
+                                </div>
+                                <div className="form-group col-md-2 col-lg-2 col-sm-12">
+                                    <label htmlFor="button">&nbsp;</label>
+                                    <button type="submit" className="btn btn-primary w-100">
+                                        <i className="fa fa-search" aria-hidden="true"></i></button>
                                 </div>
                             </div>
                         </form>
